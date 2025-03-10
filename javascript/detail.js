ad6da4a162170b0aa87afc9b36d7e3e21277020a
Lidia2509 <pali24ee@student.ju.se>
2025-03-10 13:44:26 +0100

    item added to shoping cart

--- javascript/detail.js
+++ javascript/detail.js
@@ -3,6 +3,7 @@ const buttonElement = document.getElementById("button-style");
 let rivaChair = {
   name: "Riva Chair",
   price: "SEK 2300",
+  image: "pictures-home/1.png",
 };
 
 //if you click "add to cart" button: store data and open shopping-cart.html
--- javascript/shoppingcart.js
+++ javascript/shoppingcart.js
@@ -5,14 +5,33 @@ window.onload = function () {
     //create a new element in shopping-cart.html under id:try
     const tryList = document.getElementById("try");
 
-    const listItem = document.createElement("li");
+    const listItem = document.createElement("ul");
 
     //the text in the newly created li element - put into variables
     //variables are inspired from youtube: https://www.youtube.com/watch?v=u_iQn0hvrac accessed: 10.03.25
     listItem.innerHTML = `
-        <h3>${newRivaChair.name}</h3>
-        <p>Price: ${newRivaChair.price}</p>
-        <img src="pictures-home/8.png" alt="${newRivaChair.name}" />
+
+        <figure id="first-cart-image">
+        <img class="first-article" src="pictures-home/1.png" alt="" />
+
+        <ul>
+          <li class="footer-header">${newRivaChair.name}</li>
+          <li>Brown wood chair with armrest</li>
+        </ul>
+
+        <ul>
+          <li class="footer-header">Price</li>
+          <li>${newRivaChair.price}</li>
+        </ul>
+
+        <a href="#">
+          <img
+            class="delete-icon"
+            src="icon/delete-icon.png"
+            alt="delete button"
+          />
+        </a>
+      </figure>
       `;
 
     tryList.appendChild(listItem);
@@ -23,3 +42,9 @@ window.onload = function () {
     tryList.innerHTML = "<li>No products in the cart.</li>";
   }
 };
+
+const deleteElement = document.getElementsByClassName("delete-icon");
+
+document.getElementById("delete-icon").addEventListener("click", function () {
+
+});
